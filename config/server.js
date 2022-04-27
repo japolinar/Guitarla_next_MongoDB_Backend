ef32007b75fd7a97c91f30b3323a2c63b8a61e95
japolinar <japolinar_1@hotmail.com>
2022-04-27 11:43:30 -0400

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0158d1f68be10e53ba9d2ae85160b391'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0ae8233-c313-4e62-a367-2d717ac8e590'
+};
